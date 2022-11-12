2f56228d80e6a028f3785ba744f1bc0ddcb05125
metonymyjvs <tempestcat9@yahoo.com>
2022-11-12 16:00:43 -0500

    class 11/16

---
+++ unit-11-lesson-2/download-files.sh
@@ -0,0 +1,4 @@
+wget https://upload.wikimedia.org/wikipedia/commons/c/c0/MUTCD_R1-1.svg
+wget https://upload.wikimedia.org/wikipedia/commons/f/f6/Pictogram_pedestrian.svg
+wget https://upload.wikimedia.org/wikipedia/commons/b/bd/Recreate_Free_Zone.svg
+
